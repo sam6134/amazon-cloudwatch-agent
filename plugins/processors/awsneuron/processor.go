84848e825b2009b459c539b15d21dca43c25b8a1
Aditya Purang <44022838+aditya-purang@users.noreply.github.com>
2024-03-11 18:23:59 +0000

    log json directly

--- plugins/processors/awsneuron/processor.go
+++ plugins/processors/awsneuron/processor.go
@@ -5,6 +5,7 @@ package awsneuron
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/aws/amazon-cloudwatch-agent/plugins/processors/awsneuron/internal"
 	"go.opentelemetry.io/collector/component"
@@ -57,7 +58,8 @@ func (d *awsneuronprocessor) processMetrics(ctx context.Context, md pmetric.Metr
 				m := metrics.At(k)
 				if strings.Contains(m.Name(), "neuron") && !isNeuron {
 					isNeuron = true
-					d.logMd(md, "neuron metrics object before modification")
+					jsonData, _ := json.Marshal(md)
+					d.logger.Info(fmt.Sprintf("neuron metrics object before modification : %s", string(jsonData)))
 				}
 				d.metricModifier.ModifyMetric(m).MoveAndAppendTo(newMetrics)
 			}
@@ -65,7 +67,8 @@ func (d *awsneuronprocessor) processMetrics(ctx context.Context, md pmetric.Metr
 		}
 	}
 	if isNeuron {
-		d.logMd(md, "neuron metrics object after modification")
+		jsonData, _ := json.Marshal(md)
+		d.logger.Info(fmt.Sprintf("neuron metrics object after modification : %s", string(jsonData)))
 	}
 	return md, nil
 }
@@ -80,54 +83,4 @@ func (d *awsneuronprocessor) Start(ctx context.Context, _ component.Host) error 
 	d.shutdownC = make(chan bool)
 	d.started = true
 	return nil
-}
-
-func (dc *awsneuronprocessor) logMd(md pmetric.Metrics, mdName string) {
-	var logMessage strings.Builder
-
-	logMessage.WriteString(fmt.Sprintf("\"%s\" : {\n", mdName))
-	rms := md.ResourceMetrics()
-	for i := 0; i < rms.Len(); i++ {
-		rs := rms.At(i)
-		ilms := rs.ScopeMetrics()
-		logMessage.WriteString(fmt.Sprintf("\t\"ResourceMetric_%d\": {\n", i))
-		for j := 0; j < ilms.Len(); j++ {
-			ils := ilms.At(j)
-			metrics := ils.Metrics()
-			logMessage.WriteString(fmt.Sprintf("\t\t\"ScopeMetric_%d\": {\n", j))
-			logMessage.WriteString(fmt.Sprintf("\t\t\"Metrics_%d\": [\n", j))
-
-			for k := 0; k < metrics.Len(); k++ {
-				m := metrics.At(k)
-				logMessage.WriteString(fmt.Sprintf("\t\t\t\"Metric_%d\": {\n", k))
-				logMessage.WriteString(fmt.Sprintf("\t\t\t\t\"name\": \"%s\",\n", m.Name()))
-
-				var datapoints pmetric.NumberDataPointSlice
-				switch m.Type() {
-				case pmetric.MetricTypeGauge:
-					datapoints = m.Gauge().DataPoints()
-				case pmetric.MetricTypeSum:
-					datapoints = m.Sum().DataPoints()
-				default:
-					datapoints = pmetric.NewNumberDataPointSlice()
-				}
-
-				logMessage.WriteString("\t\t\t\t\"datapoints\": [\n")
-				for yu := 0; yu < datapoints.Len(); yu++ {
-					logMessage.WriteString("\t\t\t\t\t{\n")
-					logMessage.WriteString(fmt.Sprintf("\t\t\t\t\t\t\"attributes\": \"%v\",\n", datapoints.At(yu).Attributes().AsRaw()))
-					logMessage.WriteString(fmt.Sprintf("\t\t\t\t\t\t\"value\": %v,\n", datapoints.At(yu).DoubleValue()))
-					logMessage.WriteString("\t\t\t\t\t},\n")
-				}
-				logMessage.WriteString("\t\t\t\t],\n")
-				logMessage.WriteString("\t\t\t},\n")
-			}
-			logMessage.WriteString("\t\t],\n")
-			logMessage.WriteString("\t\t},\n")
-		}
-		logMessage.WriteString("\t},\n")
-	}
-	logMessage.WriteString("},\n")
-
-	dc.logger.Info(logMessage.String())
 }
