90cde051564bb3ade74d336df1576f1025e483c7
Aditya Purang <44022838+aditya-purang@users.noreply.github.com>
2024-07-25 13:03:48 +0100

    update attribute config

--- plugins/processors/gpuattributes/internal/awsneuron_metric_modifier.go
+++ plugins/processors/gpuattributes/internal/awsneuron_metric_modifier.go
@@ -229,7 +229,6 @@ func (md *AwsNeuronMetricModifier) extractDatapointsAsMetricsAndAggregate(origin
 
 	// Creating body for the aggregated metric and add it to the new newMetricSlice for each runtime
 	for aggregatedMetricMetadata, value := range aggregatedValuesPerRuntimeTag {
-		// Aggregated metric for neuron device ecc events is not required
 		aggregatedMetric := setMetricMetadata(newMetricSlice.AppendEmpty(), aggregatedMetricMetadata.aggregatedMetricName, originalMetric.Unit())
 
 		originalMetricDatapoints.At(0).CopyTo(aggregatedMetric.SetEmptySum().DataPoints().AppendEmpty())
